a47e808d2ecca89c058b5aec26ebfc3b9568b2de
azartechnet <azartechnet@gmail.com>
2025-06-12 11:47:58 +0530

    Update index.js

--- Reactjs/project1/src/index.js
+++ Reactjs/project1/src/index.js
@@ -129,7 +129,7 @@ r1.render(React.createElement("div",null,myelem1,myelem2))*/
 
     //Task nested if else statement
 
-    import './index.css'
+    /*import './index.css'
 
     const App=()=>{
       return(
@@ -141,4 +141,63 @@ r1.render(React.createElement("div",null,myelem1,myelem2))*/
       )
     }
     const r1=ReactDOM.createRoot(document.getElementById('root'))
-    r1.render(<App/>)
+    r1.render(<App/>)*/
+
+    //Function Component
+
+    /*function Sample()
+    {
+      return(
+        <div>
+          <h1>This is Function Component</h1>
+        </div>
+      )
+    }
+    const r1=ReactDOM.createRoot(document.getElementById('root'))
+    r1.render(<Sample/>)*/
+
+    //class component
+
+    /*class Sample extends React.Component
+    {
+      render()
+      {
+        return(
+          <h1>Welcome</h1>
+        )
+      }
+    }
+    const r1=ReactDOM.createRoot(document.getElementById('root'))
+    r1.render(<Sample/>)*/
+
+    //Function Component
+
+    /*function Greeting()
+    {
+      const name="azar";
+      return(
+        <div style={{textAlign:'center',marginTop:'50px',backgroundColor:'green'}}>
+               <h1>Hello,{name}</h1>
+               <p>Welcome to React Functional component</p>
+        </div>
+      )
+    }
+    const r1=ReactDOM.createRoot(document.getElementById('root'))
+    r1.render(<Greeting/>)*/
+
+    //Using onClick()
+
+    function SimpleButton()
+    {
+      function showMessage()
+      {
+        alert("Button was clicked")
+      }
+      return(
+        <div style={{textAlign:'center',marginTop:'50px',backgroundColor:'pink'}}>
+           <button onClick={showMessage}>ClickHere</button>
+        </div>
+      )
+    }
+    const r1=ReactDOM.createRoot(document.getElementById('root'))
+    r1.render(<SimpleButton/>)
