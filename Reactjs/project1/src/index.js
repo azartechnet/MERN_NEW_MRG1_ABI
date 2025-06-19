17f6d3b67482d6fbd2c4b1a04ab7b39eddc1fbc3
azartechnet <azartechnet@gmail.com>
2025-06-19 18:48:21 +0530

    Update index.js

--- Reactjs/project1/src/index.js
+++ Reactjs/project1/src/index.js
@@ -461,7 +461,7 @@ r1.render(<Garage/>)*/
 
 //React using key
 
-function Car(props)
+/*function Car(props)
 {
   return<li>{props.id}{props.name}</li>
 }
@@ -482,5 +482,103 @@ function Garage()
       )
 }
 const r1=ReactDOM.createRoot(document.getElementById('root'))
-r1.render(<Garage/>)
+r1.render(<Garage/>)*/
+
+//Destructuring 
+
+/*function Welcome({name,age})
+{
+  return(
+    <div>
+      <h1>Welcome {name}</h1>
+      <p>Age is::{age}</p>
+    </div>
+  )
+}
+//usage
+export default function App()
+{
+  return<Welcome name="azar" age={30}/>
+}
+
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<App/>)*/
+
+import {Component} from "react";
+import { render } from "@testing-library/react";
+
+/*class Welcome extends Component
+{
+  render()
+  {
+    const {name,age}=this.props;
+    return(
+      <div>
+        <h1>Hello{name}</h1>
+        <h2>Age is {age}</h2>
+      </div>
+    )
+  }
+}
+
+export default function App()
+{
+  return(<Welcome name="azar" age={30}/>)
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<App/>)*/
+
+//Destructring state in class component
+
+/*class Profile extends React.Component
+{
+  constructor()
+  {
+    super();
+    this.state={
+      name:"azar",
+      age:30,
+  };
+}
+  render()
+  {
+    const {name,age}=this.state;
+    return(
+      <div>
+        <h2>Name:{name}</h2>
+        <h3>Age is::{age}</h3>
+      </div>
+    )
+  }
+  }
+
+export default Profile;
+
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Profile/>)*/
+
+//Destructuring in Arrays
+
+function Skills()
+{
+  const skills=["java","python","c++"];
+  const [first,second,third]=skills;
+  return(
+    <div>
+      <h3>MySkills</h3>
+      <ul>
+        <li>{first}</li>
+        <li>{second}</li>
+        <li>{third}</li>
+      </ul>
+    </div>
+    )
+}
+export default Skills;
+
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Skills/>)
+
+
+
 
