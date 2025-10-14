514aebc69f69a0203081cb377d5a08508f3030df
azartechnet <azartechnet@gmail.com>
2025-10-14 12:09:16 +0530

    updated

---
+++ crudproject/client/src/CrudPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+function CrudPage()
+{
+    return(
+        <div className="container mt-4">
+            <h1 className="text-center">CrudPage</h1>
+            <p className="text-center">This is CrudPage Page</p>
+        </div>
+    )
+}
+export default CrudPage;
---
+++ crudproject/client/src/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Home()
+{
+    return(
+        <div className="container mt-4">
+            <h1 className="text-center">HomePage</h1>
+            <p className="text-center">This is Home Page</p>
+        </div>
+    )
+}
+export default Home;
