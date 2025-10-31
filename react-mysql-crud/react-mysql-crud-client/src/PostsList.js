688dc45dd5bd3cd254c646f726f162a393970c4a
azartechnet <azartechnet@gmail.com>
2025-10-31 12:15:32 +0530

    updated

--- react-mysql-crud/react-mysql-crud-client/src/PostsList.js
+++ react-mysql-crud/react-mysql-crud-client/src/PostsList.js
@@ -1,7 +1,47 @@
 import React from "react";
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+import { useState } from "react";
+import { useEffect } from "react";
+
 const PostsList=()=>{
+    const [posts,setPosts]=useState([]);
+    useEffect(()=>{
+        axios.get('http://localhost:3001/getposts')
+        .then(response=>setPosts(response.data))
+    },[])
+
     return(
-        <h1>This is PostsList</h1>
+         <div className="container mt-5">
+              <h2>PostsList</h2>
+              <table className="table table-striped">
+                  <thead>
+                    <tr>
+                       <th>Id</th>
+                       <th>title</th>
+                       <th>Body</th>
+                       <th>Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                        posts.map(
+                            post=>(
+                                <tr key={post.id}>
+                                  <td>{post.id}</td>
+                                  <td>{post.title}</td>
+                                  <td>{post.body}</td>
+                                  <td>
+                                    <Link to={""} className="btn btn-warning btn-sm mr-2">Edit</Link>
+                                    <button onClick={""} className="btn btn-danger btn-sm">Delete</button> 
+                                  </td>
+                                </tr>
+                            )
+                        )
+                    }
+                  </tbody>
+              </table>
+         </div>   
     )
 }
 export default PostsList;
---
+++ react-mysql-crud/server.js
@@ -0,0 +1,58 @@
+const express=require('express')
+const mysql=require('mysql')
+const bodyparse=require('body-parser')
+const cors=require('cors')
+
+const app=express()
+const port=3001;
+
+//Middleware
+
+app.use(cors())
+app.use(bodyparse.json())
+
+//MySQL
+const db=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'dharanyadb'
+})
+db.connect((err)=>{
+    if(!err)
+        console.log('DB Connected..')
+    else
+        console.log('DB not connected..')
+})
+//create table
+app.get("/createtable",(req,res)=>{
+
+    let sql='create table posts(id int auto_increment,title varchar(100),body text,primary key(id))';
+    db.query(sql,(err,results)=>{
+        if(!err)
+            console.log('Table created')
+        res.send('Table Created')
+    })
+
+})
+// Insert post
+app.post('/addpost', (req, res) => {
+    let post = { title: req.body.title, body: req.body.body };
+    let sql = 'INSERT INTO posts SET ?';
+    db.query(sql, post, (err, result) => {
+        if (err) throw err;
+        res.send('Post added...');
+    });
+});
+//getPosts
+app.get("/getposts",(req,res)=>{
+    let sql="select * from posts";
+    db.query(sql,(err,result)=>{
+        if(err)
+            throw err;
+        res.send(result)
+    })
+})
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
